d72ab049fd31ca662231bcf3c02c8d9d8902f35e
aquabtw <dimaaskarp@mail.ru>
2024-04-23 11:48:09 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, render_template, redirect, session
+from models.user import User, find_user
+from shared.db import db
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "abS8gfjAh"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db.init_app(app)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/index")
+def index():
+    user = find_user(session["email"])
+    return render_template("index.html", user=user)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        if username and email and password:
+            if find_user(email):
+                return render_template("register.html", error="Электронная почта занята.")
+
+            new_user = User(username, email, password)
+
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/login")
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = find_user(email)
+
+        if user and user.check_password(password):
+            session["email"] = user.email
+            return redirect("/index")
+        else:
+            return render_template("login.html", error="Неверная почта или пароль.")
+
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models/user.py
@@ -0,0 +1,22 @@
+import bcrypt
+
+from shared.db import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
+
+
+def find_user(email: str) -> User:
+    return User.query.filter_by(email=email).first()

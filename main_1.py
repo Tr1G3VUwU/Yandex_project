23364655ac4caf37d1f07e8193ae3f7a258ff36d
Tr1G3V <liberbuk@gmail.com>
2024-04-24 14:01:03 +0300

    Add file

--- main_1.py
+++ main_1.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 import os
 
 app = Flask(__name__)
@@ -8,26 +9,31 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/iri', methods=['GET'])
 def index():
+    con = sqlite3.connect("static\Base_with_manga_and_novel\data.sqlite")
+    cur = con.cursor()
+    result = cur.execute("""SELECT * FROM Manga_DB""").fetchall()
+    sorted_list = sorted(result, key=lambda x: x[-1])
     The_proposed_title_1 = {}
-    The_proposed_title_1['link_1'] = os.path.basename("static\Base_with_manga_and_novel\jujutsu_kaisen")
-    The_proposed_title_1['Name_1'] = (
-        open('static\Base_with_manga_and_novel\jujutsu_kaisen\Title_name.txt', 'r', encoding="utf-8").readline())
-    The_proposed_title_1['Authors_name_1'] = (
-        open('static\Base_with_manga_and_novel\jujutsu_kaisen\Authors_name.txt', 'r', encoding="utf-8").readline())
-    The_proposed_title_1['Description_of_the_title_1'] = (
-        open('static\Base_with_manga_and_novel\jujutsu_kaisen\Description_of_the_title.txt', 'r',
-             encoding="utf-8").readline())
-    The_proposed_title_1['estimation_1'] = (
-        open('static\Base_with_manga_and_novel\jujutsu_kaisen\estimation.txt', 'r', encoding="utf-8").readline())
-    The_proposed_title_1['Img_1'] = 'static\Base_with_manga_and_novel\jujutsu_kaisen\img_cover.jpeg'
+    The_proposed_title_1['link_1'] = sorted_list[0][5]
+    The_proposed_title_1['Name_1'] = sorted_list[0][2]
+    The_proposed_title_1['Authors_name_1'] = sorted_list[0][1]
+    The_proposed_title_1['Description_of_the_title_1'] = sorted_list[0][4]
+    The_proposed_title_1['estimation_1'] = sorted_list[0][3]
+    The_proposed_title_1['Img_1'] = f'static\Base_with_manga_and_novel\{sorted_list[0][6]}\img_cover.jpeg'
+    con.close()
     return render_template('index.html', **The_proposed_title_1)
 
 
 @app.route('/jujutsu_kaisen', methods=['GET'])
 def bebra():
+    con = sqlite3.connect("static\Base_with_manga_and_novel\data.sqlite")
+    cur = con.cursor()
+    result = cur.execute("""SELECT * FROM Manga_DB""").fetchall()
+    result = cur.execute(f"""UPDATE Manga_DB SET Estimation = {result[0][3] + 1} WHERE ID = 1""").fetchall()
+    con.close()
     lists = ['002_1nZi.jpeg', '003_RrQF.jpeg']
     return render_template('Reading.html', listik = lists)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(debug=True)
